673468ee85f6f048f9e98b9b8729ac6c9f3e8781
rupeshtiwari <roopkt@gmail.com>
2021-03-11 16:28:27 -0500

    feat: adding project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_cosmos_db_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
